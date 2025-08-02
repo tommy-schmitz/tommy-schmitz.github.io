1bf78992d0aad70435d575d0c9a5c9fe2e7e8220
Tommy <Tommy>
2025-08-01 18:41:37 -0700

    more progress

--- data_app/main.js
+++ data_app/main.js
@@ -51,10 +51,16 @@ const RELAY_SERVER_URL = (() => {
   }
 })();
 
-const get_master_public_key = async(ui) => {
+const get_master_keys = async(ui) => {
   const maybe_public_key = localStorage.getItem('master_public_key');
-  if(maybe_public_key !== null) {
-    return await window.crypto.subtle.importKey('jwk', JSON.parse(maybe_public_key), {name: "ECDSA", namedCurve: "P-384"}, true, ['verify']);
+  const maybe_private_key = localStorage.getItem('master_private_key');
+  if(maybe_public_key !== null  &&  maybe_private_key !== null) {
+    return {
+      master_public_key: await window.crypto.subtle.importKey('jwk', JSON.parse(maybe_public_key),
+                                                              {name: "ECDSA", namedCurve: "P-384"}, true, ['verify']),
+      master_private_key: await window.crypto.subtle.importKey('jwk', JSON.parse(maybe_private_key),
+                                                               {name: "ECDSA", namedCurve: "P-384"}, true, ['sign']),
+    };
   }
 
   ui.main_page_body.innerText = 'Click here to generate your cryptographic keys, which will be stored in your web browser: ';
@@ -69,7 +75,7 @@ const get_master_public_key = async(ui) => {
   const keys = await window.crypto.subtle.generateKey({name: "ECDSA", namedCurve: "P-384"}, true, ['sign', 'verify']);
   localStorage.setItem('master_public_key', JSON.stringify(await window.crypto.subtle.exportKey('jwk', keys.publicKey)));
   localStorage.setItem('master_private_key', JSON.stringify(await window.crypto.subtle.exportKey('jwk', keys.privateKey)));
-  return keys.publicKey;
+  return {master_private_key: keys.privateKey, master_public_key: keys.publicKey};
 };
 
 const export_public_key = async(public_key) => {
@@ -79,13 +85,19 @@ const export_public_key = async(public_key) => {
 const page_load_promise = new Promise((resolve, reject) => (window.addEventListener('load', resolve)));
 
 const get_keys = async({ui, sock}) => {
-  const public_key = await get_master_public_key();
-  const public_jwk_string = await export_public_key(public_key);
+  const {master_public_key, master_private_key} = await get_master_keys(ui);
+  const public_jwk_string = await export_public_key(master_public_key);
+  console.log('tommy 1');
   await sock.emit('the id is', public_jwk_string);
 
   const maybe_partner_key = localStorage.getItem('partner_key');
   if(maybe_partner_key !== null) {
-    return await window.crypto.subtle.importKey('jwk', JSON.parse(maybe_partner_key), {name: 'ECDSA', namedCurve: 'P-384'}, true, ['verify']);
+    return {
+      master_public_key,
+      master_private_key,
+      partner_key: await window.crypto.subtle.importKey('jwk', JSON.parse(maybe_partner_key),
+                                                        {name: 'ECDSA', namedCurve: 'P-384'}, true, ['verify']),
+    };
   }
 
   const readonly_textarea = document.createElement('textarea');
@@ -129,11 +141,32 @@ const get_keys = async({ui, sock}) => {
       const partner_key = await window.crypto.subtle.importKey('jwk', JSON.parse(editable_textarea.value),
                                                                {name: "ECDSA", namedCurve: "P-384"}, true, ['verify']);
       localStorage.setItem('partner_key', editable_textarea.value);
-      return {public_key, partner_key};
+      return {master_public_key, master_private_key, partner_key};
     } catch(_) {
       feedback_div.innerText = "Error loading partner's key. Try again.";
     }
   }
+};
+
+const digitally_sign = async(private_key, data) => {
+  const temp = await window.crypto.subtle.sign({name: 'ECDSA', hash: {name: 'SHA-384'}},
+                                               private_key, new TextEncoder().encode(JSON.stringify(data)));
+  const bytes = new Uint8Array(temp);
+  let binary = '';
+  for (const byte of bytes) {
+    binary += String.fromCharCode(byte);
+  }
+  return binary;
+};
+
+const check_signature = async(public_key, data, signature) => {
+  const len = signature.length;
+  const buffer = new ArrayBuffer(len);
+  const bytes = new Uint8Array(buffer);
+  for(let i = 0; i<len; i++)
+    bytes[i] = signature.charCodeAt(i);
+  return await window.crypto.subtle.verify({name: 'ECDSA', hash: {name: 'SHA-384'}},
+                                           public_key, buffer, new TextEncoder().encode(JSON.stringify(data)));
 };
 
 const main = async() => {
@@ -143,14 +176,37 @@ const main = async() => {
   sock.on('disconnect', () => {
     console.log('Disconnected.');
   });
+  const interlocutor_available_promise = new Promise((resolve, reject) => (sock.on('interlocutor is available', resolve)));
+  const real_connect_promise = new Promise((resolve, reject) => (sock.on('real_connect', resolve)));
   await new Promise((resolve, reject) => (sock.on('connect', resolve)));
+  await real_connect_promise;
   console.log('Connected.');
 
   await page_load_promise;
 
   const ui = initialize_ui();
 
-  const {} = await get_keys({ui, sock});
+  const {master_private_key, master_public_key, partner_key} = await get_keys({ui, sock});
+
+  const temp_keys = await window.crypto.subtle.generateKey({name: "ECDH", namedCurve: "P-384"}, false, ['deriveKey']);
+
+  sock.on('interlocutor says', async(message) => {
+    if(message.type === 'set public key') {
+      const {data, signature} = message;
+      console.log('signature valid?', await check_signature(partner_key, data, signature));
+    } else {
+      // Do nothing
+    }
+  });
+
+  sock.emit('desired interlocutor is', await export_public_key(partner_key));
+
+  console.log('waiting for interlocutor');
+
+  await interlocutor_available_promise;
+
+  const data = {public_key: await export_public_key(temp_keys.publicKey), timestamp: Date.now()};
+  sock.emit('interlocutor should hear', {type: 'set public key', data, signature: digitally_sign(master_private_key, data)});
 
   ui.main_page_body.innerText = 'reached the end of the program';
 };
