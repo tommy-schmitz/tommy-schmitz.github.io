1004658d347f80b4b2be243ee7149d16c85869b0
Tommy <Tommy>
2025-08-01 17:09:14 -0700

    implement saving of partner's public key

--- data_app/main.js
+++ data_app/main.js
@@ -78,6 +78,64 @@ const export_public_key = async(public_key) => {
 
 const page_load_promise = new Promise((resolve, reject) => (window.addEventListener('load', resolve)));
 
+const get_keys = async({ui, sock}) => {
+  const public_key = await get_master_public_key();
+  const public_jwk_string = await export_public_key(public_key);
+  await sock.emit('the id is', public_jwk_string);
+
+  const maybe_partner_key = localStorage.getItem('partner_key');
+  if(maybe_partner_key !== null) {
+    return await window.crypto.subtle.importKey('jwk', JSON.parse(maybe_partner_key), {name: 'ECDSA', namedCurve: 'P-384'}, true, ['verify']);
+  }
+
+  const readonly_textarea = document.createElement('textarea');
+  readonly_textarea.setAttribute('readonly', '');
+  Object.assign(readonly_textarea.style, {
+    backgroundColor: '#dddddd',
+    width: '800px',
+    height: '40px',
+  });
+  readonly_textarea.innerText = public_jwk_string;
+
+  const editable_textarea = document.createElement('textarea');
+  Object.assign(editable_textarea.style, {
+    width: '800px',
+    height: '40px',
+  });
+
+  const save_button = document.createElement('button');
+  save_button.innerText = 'Save';
+  const make_button_promise = () => (new Promise((resolve, reject) => (save_button.addEventListener('click', resolve))));
+
+  const feedback_div = document.createElement('div');
+
+  ui.main_page_body.innerText = '';
+  ui.main_page_body.appendChild(document.createTextNode('Your public key is'));
+  ui.main_page_body.appendChild(document.createElement('br'));
+  ui.main_page_body.appendChild(readonly_textarea);
+  ui.main_page_body.appendChild(document.createElement('br'));
+  ui.main_page_body.appendChild(document.createTextNode('Copy and paste your public key somewhere where your partner can find it.'));
+  ui.main_page_body.appendChild(document.createElement('br'));
+  ui.main_page_body.appendChild(document.createTextNode("Next, paste your partner's public key in here:"));
+  ui.main_page_body.appendChild(document.createElement('br'));
+  ui.main_page_body.appendChild(editable_textarea);
+  ui.main_page_body.appendChild(document.createElement('br'));
+  ui.main_page_body.appendChild(save_button);
+  ui.main_page_body.appendChild(feedback_div);
+
+  while(true) {
+    await make_button_promise();
+    try {
+      const partner_key = await window.crypto.subtle.importKey('jwk', JSON.parse(editable_textarea.value),
+                                                               {name: "ECDSA", namedCurve: "P-384"}, true, ['verify']);
+      localStorage.setItem('partner_key', editable_textarea.value);
+      return {public_key, partner_key};
+    } catch(_) {
+      feedback_div.innerText = "Error loading partner's key. Try again.";
+    }
+  }
+};
+
 const main = async() => {
   console.log('Version 2025-08-01 16:31');
   const socket_io = await initialize_socket_io();
@@ -92,21 +150,9 @@ const main = async() => {
 
   const ui = initialize_ui();
 
-  const public_key = await get_master_public_key(ui);
-  const public_jwk_string = await export_public_key(public_key);
+  const {} = await get_keys({ui, sock});
 
-  await sock.emit('the id is', public_jwk_string);
-
-  ui.main_page_body.innerHTML = 'Your public key is<br />';
-  const readonly_textarea = document.createElement('textarea');
-  readonly_textarea.setAttribute('readonly', '');
-  Object.assign(readonly_textarea.style, {
-    backgroundColor: '#dddddd',
-    width: '800px',
-    height: '40px',
-  });
-  readonly_textarea.innerText = public_jwk_string;
-  ui.main_page_body.appendChild(readonly_textarea);
+  ui.main_page_body.innerText = 'reached the end of the program';
 };
 
 main();
