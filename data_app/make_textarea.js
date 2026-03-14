d2ed300ab3007bd9724ee5ec1570771f391eb6c3
Tommy <Tommy>
2026-03-14 13:23:50 -0700

    increase sophistication of text editor

--- data_app/make_textarea.js
+++ data_app/make_textarea.js
@@ -52,7 +52,8 @@ const make_textarea = ({on_change, on_undo, on_redo}) => {
   let prev_value = '';
   let start;
   let end;
-  let cursor = 0;
+  let selection_start = 0;
+  let selection_end = 0;
 
   const editor = document.createElement('div');
   editor.style.cssText = `
@@ -76,23 +77,145 @@ const make_textarea = ({on_change, on_undo, on_redo}) => {
       else
         return '';
     })();
-    editor.textContent = prev_value.slice(0, cursor) + maybe_cursor + prev_value.slice(cursor);
+    const selection_0 = Math.min(selection_start, selection_end);
+    const selection_1 = Math.max(selection_start, selection_end);
+    editor.textContent = (
+        prev_value.slice(0, selection_0)
+      + maybe_cursor + prev_value.slice(selection_0, selection_1)
+      + maybe_cursor + prev_value.slice(selection_1)
+    );
   };
 
-  editor.addEventListener('keydown', (e) => {
-    e.preventDefault();
-    if (e.key === 'Backspace' && cursor > 0) {
-      prev_value = prev_value.slice(0, cursor - 1);
-      cursor--;
-    } else if (e.key === 'ArrowLeft' && cursor > 0) {
-      cursor--;
-    } else if (e.key === 'ArrowRight' && cursor < prev_value.length) {
-      cursor++;
-    } else if (e.key.length === 1) {
-      prev_value = prev_value.slice(0, cursor) + e.key + prev_value.slice(cursor);
-      cursor++;
+  editor.addEventListener('keydown', async(e) => {
+    const specials = [
+      e.key === 'z' && (e.ctrlKey || e.metaKey) && !e.shiftKey,
+      (e.key === 'y' || (e.key === 'z' && e.shiftKey)) && (e.ctrlKey || e.metaKey),
+      e.key === 'c' && (e.ctrlKey || e.metaKey),
+      e.key === 'v' && (e.ctrlKey || e.metaKey),
+      e.key === 'x' && (e.ctrlKey || e.metaKey),
+      e.key === 'Home' && e.ctrlKey,
+      e.key === 'End' && e.ctrlKey,
+    ];
+    const [isUndo, isRedo, isCopy, isPaste, isCut] = specials;
+
+    if((e.ctrlKey || e.metaKey || e.altKey) && !specials.some((x)=>(x)))
+      return;
+
+    let not_recognized = false;
+
+    if(isUndo) {
+      on_undo();
+    } else if(isRedo) {
+      on_redo();
+    } else if(isCopy) {
+      const selection_0 = Math.min(selection_start, selection_end);
+      const selection_1 = Math.max(selection_start, selection_end);
+      navigator.clipboard.writeText(prev_value.slice(selection_0, selection_1));
+    } else if(isPaste) {
+      const to_insert = await navigator.clipboard.readText();
+      const selection_0 = Math.min(selection_start, selection_end);
+      const selection_1 = Math.max(selection_start, selection_end);
+      prev_value = prev_value.slice(0, selection_0) + to_insert + prev_value.slice(selection_1);
+      selection_end = selection_start = selection_0 + to_insert.length;
+    } else if(isCut) {
+      const selection_0 = Math.min(selection_start, selection_end);
+      const selection_1 = Math.max(selection_start, selection_end);
+      navigator.clipboard.writeText(prev_value.slice(selection_0, selection_1));
+      prev_value = prev_value.slice(0, selection_0) + prev_value.slice(selection_1);
+      selection_end = selection_start = selection_0;
+    } else if(selection_end !== selection_start  &&  (e.key==='Backspace' || e.key==='Delete')) {
+      const selection_0 = Math.min(selection_start, selection_end);
+      const selection_1 = Math.max(selection_start, selection_end);
+      prev_value = prev_value.slice(0, selection_0) + prev_value.slice(selection_1);
+      selection_end = selection_start = selection_0;
+    } else if(e.key === 'Backspace' && selection_start > 0) {
+      prev_value = prev_value.slice(0, selection_start - 1) + prev_value.slice(selection_start);
+      selection_start -= 1;
+      selection_end = selection_start
+    } else if(e.key === 'Delete' && selection_start < prev_value.length) {
+      prev_value = prev_value.slice(0, selection_start) + prev_value.slice(selection_start + 1);
+    } else if(e.key === 'ArrowLeft' && selection_end > 0 && !e.ctrlKey) {
+      selection_end -= 1;
+      if(!e.shiftKey)
+        selection_start = selection_end;
+    } else if(e.key === 'ArrowDown') {
+      const origin = selection_end;
+      for(; selection_end>0; --selection_end)
+        if(prev_value[selection_end-1] === '\n')
+          break;
+      const distance = origin - selection_end;
+      for(selection_end = origin; selection_end<prev_value.length; ++selection_end)
+        if(prev_value[selection_end] === '\n')
+          break;
+      if(selection_end < prev_value.length)
+        selection_end += 1;
+      const candidate = Math.min(selection_end + distance, prev_value.length);
+      for(; selection_end<candidate; ++selection_end)
+        if(prev_value[selection_end] === '\n')
+          break;
+      if(!e.shiftKey)
+        selection_start = selection_end;
+    } else if(e.key === 'ArrowUp') {
+      const origin = selection_end;
+      for(; selection_end>0; --selection_end)
+        if(prev_value[selection_end-1] === '\n')
+          break;
+      const distance = origin - selection_end;
+      if(selection_end > 0)
+        selection_end -= 1;
+      for(; selection_end>0; --selection_end)
+        if(prev_value[selection_end-1] === '\n')
+          break;
+      const candidate = Math.min(selection_end + distance, prev_value.length);
+      for(; selection_end<candidate; ++selection_end)
+        if(prev_value[selection_end] === '\n')
+          break;
+      if(!e.shiftKey)
+        selection_start = selection_end;
+    } else if(e.key === 'ArrowRight' && selection_end < prev_value.length && !e.ctrlKey) {
+      selection_end += 1;
+      if(!e.shiftKey)
+        selection_start = selection_end;
+    } else if(e.key.length === 1) {
+      const selection_0 = Math.min(selection_start, selection_end);
+      const selection_1 = Math.max(selection_start, selection_end);
+      prev_value = prev_value.slice(0, selection_0) + e.key + prev_value.slice(selection_1);
+      selection_end = selection_start = selection_0 + 1;
+    } else if(e.key === 'End' && !e.ctrlKey) {
+      for(; selection_end<prev_value.length; ++selection_end)
+        if(prev_value[selection_end] === '\n')
+          break;
+      if(!e.shiftKey)
+        selection_start = selection_end;
+    } else if(e.key === 'Home' && !e.ctrlKey) {
+      for(; selection_end>0; --selection_end)
+        if(prev_value[selection_end-1] === '\n')
+          break;
+      if(!e.shiftKey)
+        selection_start = selection_end;
+    } else if(e.key === 'End' && e.ctrlKey) {
+      selection_end = prev_value.length;
+      if(!e.shiftKey)
+        selection_start = selection_end;
+    } else if(e.key === 'Home' && e.ctrlKey) {
+      selection_end = 0;
+      if(!e.shiftKey)
+        selection_start = selection_end;
+    } else if(e.key === 'Enter') {
+      const selection_0 = Math.min(selection_start, selection_end);
+      const selection_1 = Math.max(selection_start, selection_end);
+      prev_value = prev_value.slice(0, selection_0) + '\n' + prev_value.slice(selection_1);
+      selection_end = selection_start = selection_0 + 1;
+    } else {
+      not_recognized = true;
     }
-    render();
+
+    if(not_recognized) {
+      // Do nothing
+    } else {
+      e.preventDefault();
+      render();
+    }
   });
 
   editor.addEventListener('focus', render);
